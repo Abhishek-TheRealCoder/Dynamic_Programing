e662cca9191d5a93ba25ca39abedf2681cb0530d
Abhishek-TheRealCoder <abhisheksingh11102003@gmail.com>
2026-01-10 22:30:16 +0530

    Printing longest Divisible subset

--- LIS-Variants/PrintLargestDivisibleSubset.cpp
+++ LIS-Variants/PrintLargestDivisibleSubset.cpp
@@ -5,16 +5,15 @@ vector<int> largestDivisibleSubset(vector<int>& nums) {
     sort(nums.begin(), nums.end());
     int n = nums.size();
     vector<int>dp(n,1);
-    vector<int>prevIdx(n,-1);
+    vector<int>path(n,-1);
     int maxiIdx = 0;
     int maxi = 0;
 
     for(int i = 1;i < n;i++){
-        for(int j = 0;j < i;j++){
-            if(nums[i] % nums[j] == 0){
-                if(dp[i] > 1 + dp[j])continue;
-                dp[i] = max(dp[i],1+dp[j]);
-                prevIdx[i] = j;
+        for(int j = i - 1;j >= 0;j--){
+            if(nums[i] % nums[j] == 0 && 1 + dp[j] > dp[i]){
+                dp[i] = 1 + dp[j];
+                path[i] = j;
             }
 
             if(dp[i] > maxi){
@@ -26,13 +25,14 @@ vector<int> largestDivisibleSubset(vector<int>& nums) {
     vector<int>ans;
     while(maxiIdx != -1){
         ans.push_back(nums[maxiIdx]);
-        maxiIdx = prevIdx[maxiIdx];
+        maxiIdx = path[maxiIdx];
     }
+    reverse(ans.begin(),ans.end());
     return ans;
 }
 
 int main(){
-    vector<int>arr={1,4,2,8};
+    vector<int>arr={3,4,15,32,64};
     vector<int>ans = largestDivisibleSubset(arr);
     for(auto x:ans){
         cout<<x<<" ";
